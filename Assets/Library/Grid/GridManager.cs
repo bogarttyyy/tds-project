fc3f07e7f7fd1b766b76aad5bab7759fc3adab28
Dennis Abad <dennis.abad6@gmail.com>
2023-03-14 10:16:02 +1100

    Added Simple Grid system

---
+++ Assets/Library/Grid/GridManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid(4, 4, 2f);
+    }
+}
